2351495d1658b7483b8bce142e1c33103fa40011
Bifurcar <IanEzequielG@pm.me>
2019-08-12 18:14:44 -0300

    Generar migraciones

---
+++ database/migrations/2019_08_12_211104_create_ventas_detalle_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentasDetalleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas_detalle', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas_detalle');
+    }
+}
