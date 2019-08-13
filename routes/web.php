a158eb46107b1805abb012d11abe4e8ab3afddc2
forkRebel <IanEzequielG@pm.me>
2019-08-13 12:31:02 -0300

    Crear controladores y generar rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::middleware(['auth'])->group(function() {
+    Route::resource('articulos-insumos', 'Articulo_InsumoController');
+    Route::resource('articulos', 'ArticuloController');
+    Route::resource('clientes', 'ClienteController');
+    Route::resource('proveedores', 'ProveedorController');
+    Route::resource('remitos-cabecera', 'Remito_CabeceraController');
+    Route::resource('remitos-detalle', 'Remito_DetalleController');
+    Route::resource('stock-cabecera', 'Stock_CabeceraController');
+    Route::resource('stock-detalle', 'Stock_DetalleController');
+    Route::resource('users', 'UserController');
+    Route::resource('ventas-cabecera', 'Venta_CabeceraController');
+    Route::resource('ventas-detalle', 'Venta_DetalleController');
+});
 
+Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
